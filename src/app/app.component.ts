a6d178dce8923007cd6d069003eb10dc1b1593a7
9syncopation <9syncopation@gmail.com>
2020-06-06 16:50:04 +0300

    layout setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+// decorator
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',// the app root, injects the code
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstangular';
-}
+  title: string = 'firstAngular';
+  date =new Date();
+  name:string = 'Eyal';
+
+ }
