61c20024b18142dc51682c6c80be75f4a455d5c0
9syncopation <9syncopation@gmail.com>
2020-06-06 17:18:08 +0300

    functionality >>AddItem, Delete Item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import { isDate } from 'util';
 
 // decorator
 @Component({
-  selector: 'app-root',// the app root, injects the code
+  selector: 'app-root', // the app root, injects the code
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title: string = 'firstAngular';
-  date =new Date();
-  name:string = 'Eyal';
+  todoValue: string;
+  list: Todo[];
 
- }
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = '';
+  }
+
+  addItem() {
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
+}
